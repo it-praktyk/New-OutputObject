bd93e67289da9361189fb90cad57f63aafb8c7f0
Wojciech Sciesinski <wojciech@sciesinski.net>
2016-06-06 23:40:50 +0200

    2016-06-06 - initial release, doesn't work yet

---
+++ Functions/Test-CharsInPath.Tests.ps1
@@ -0,0 +1,34 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Test-CharsInPath" {
+    
+    Context "Input is a file or a directory PSObject" {
+        
+        $TestFile = New-Item -Path "TestDrive:" -Name "TestFile1.txt" -ItemType File
+        
+        $TestDir = New-Item -Path "TestDrive:" -Name "TestDir1" -ItemType Container
+        
+        It "Input is a directory, SkipCheckCharsInPath" {
+            
+            Test-CharsInPath -path $TestDir -SkipCheckCharsInPath | Should Be 1
+            
+        }
+        
+        It "Input is a file, SkipCheckCharsInFileName" {
+            
+            Test-CharsInPath -path $TestFile -SkipCheckCharsInFileName | Should Be 1
+            
+        }
+        
+    }
+    <#
+    Context "Input is a string" {
+        
+        It
+        
+    }
+    
+    #>
+}
---
+++ Functions/Test-CharsInPath.ps1
@@ -0,0 +1,78 @@
+﻿Function Test-CharsInPath {
+    
+<#	
+
+    .SYNOPSIS
+
+    .DESCRIPTION
+    
+    Exit codes
+    
+    - 0 - everything OK
+    - 1 - nothing to check
+    - 
+
+    .PARAMETER Path
+    
+    .PARAMETER SkipCheckCharsInPath
+    
+    .PARAMETER SkipCheckCharsInFileName
+
+    .EXAMPLE
+
+    .LINK
+    https://github.com/it-praktyk/New-OutputObject
+
+    .LINK
+    https://www.linkedin.com/in/sciesinskiwojciech
+
+    .NOTES
+    AUTHOR: Wojciech Sciesinski, wojciech[at]sciesinski[dot]net
+    KEYWORDS: PowerShell, FileSystem
+
+    VERSIONS HISTORY
+
+#>
+    
+    [cmdletbinding()]
+    param (
+        
+        [parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        $Path,
+        [parameter(Mandatory = $false)]
+        [switch]$SkipCheckCharsInPath,
+        [parameter(Mandatory = $false)]
+        [switch]$SkipCheckCharsInFileName
+    
+    )
+    
+    $PathInvalidChars = [System.IO.Path]::GetInvalidPathChars()
+    
+    $FileInvalidChars = [System.IO.Path]::GetInvalidFileNameChars()
+    
+    $PathType = ($Path.GetType()).Name
+    
+    If (@('DirectoryInfo', 'FileInfo') -contains $PathType) {
+        
+        If ($SkipCheckCharsInPath.IsPresent -and $PathType -eq 'DirectoryInfo') {
+            
+            Return 1
+            
+        }
+        
+        If ($SkipCheckCharsInFileName.IsPresent -and $PathType -eq 'FileInfo') {
+            
+            Return 1
+            
+        }
+        
+        
+    }
+    
+    
+    
+}
+
+
+
