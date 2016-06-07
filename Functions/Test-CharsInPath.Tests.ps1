f03d92c25f7b87c865758f723f102c322fa9a30a
Wojciech Sciesinski <wojciech@sciesinski.net>
2016-06-07 23:55:35 +0200

    0.3.0 - 2016-06-07 - The first working version :-)

--- Functions/Test-CharsInPath.Tests.ps1
+++ Functions/Test-CharsInPath.Tests.ps1
@@ -1,76 +1,107 @@
-﻿Import-Module ActiveDirectory
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
-. "$here\$sut"
-
-Describe "Test-CharsInPath" {
+﻿
     
-    Context "Input is a file or a directory PSObject" {
+    $here = Split-Path -Parent $MyInvocation.MyCommand.Path
+    
+    $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+    
+    . "$here\$sut"
+    
+    [Bool]$VerboseFunctionOutput = $false
+    
+    Describe "Test-CharsInPath" {
         
-        $TestFile = New-Item -Path "TestDrive:" -Name "TestFile1.txt" -ItemType File
-        
-        $TestDir = New-Item -Path "TestDrive:" -Name "TestDir1" -ItemType Container
-        
-        It "Input is a directory, SkipCheckCharsInPath" {
+        Context "Input is a file or a directory PSObject" {
             
-            Test-CharsInPath -path $TestDir -SkipCheckCharsInPath | Should Be 1
+            $TestFile = New-Item -Path "TestDrive:" -Name "TestFile1.txt" -ItemType File
+            
+            $TestDir = New-Item -Path "TestDrive:" -Name "TestDir1" -ItemType Container
+            
+            It "Input is a directory, SkipCheckCharsInPath" {
+                
+                Test-CharsInPath -path $TestDir -SkipCheckCharsInPath | Should Be 1
+                
+            }
+            
+            It "Input is a file, SkipCheckCharsInFileName" {
+                
+                Test-CharsInPath -path $TestFile -SkipCheckCharsInFileName | Should Be 1
+                
+            }
             
         }
         
-        It "Input is a file, SkipCheckCharsInFileName" {
+        Context "Input is a string" {
             
-            Test-CharsInPath -path $TestFile -SkipCheckCharsInFileName | Should Be 1
+            [String]$CorrectPathString = 'C:\Windows\Temp\Add-ADGroupMember.ps1'
+            
+            [String]$InCorrectPathString = 'C:\Win>dows\Te%mp\Add-ADGroupMember.ps1'
+            
+            [String]$InCorrectFileNameString = 'C:\Windows\Temp\Ard-ADGrou|p<Member.ps1'
+            
+            [String]$IncorrectFullPathString = 'C:\Win>dows\Temp\Ard-ADGrou|p<Member.ps1'
+            
+            It "Input is string, CorrectPathString" {
+                
+                Test-CharsInPath -Path $CorrectPathString -verbose:$VerboseFunctionOutput | should be 0
+                
+            }
+            
+            It "Input is string, SkipCheckCharsInPath, CorrectPathString" {
+                
+                Test-CharsInPath -Path $CorrectPathString -SkipCheckCharsInPath -verbose:$VerboseFunctionOutput | should be 0
+                
+            }
+            
+            It "Input is string, SkipCheckCharsInFileName, CorrectPathString" {
+                
+                Test-CharsInPath -Path $CorrectPathString -SkipCheckCharsInFileName -verbose:$VerboseFunctionOutput | should be 0
+                
+            }
+            
+            It "Input is string, InCorrectPathString" {
+                
+                Test-CharsInPath -Path $InCorrectPathString -SkipCheckCharsInPath -verbose:$VerboseFunctionOutput | should be 0
+                
+            }
+            
+            
+            It "Input is string, SkipCheckCharsInPath, InCorrectPathString" {
+                
+                Test-CharsInPath -Path $InCorrectPathString -SkipCheckCharsInPath -verbose:$VerboseFunctionOutput | should be 0
+                
+            }
+            
+            It "Input is string, SkipCheckCharsInFileName, InCorrectFileNameString" {
+                
+                Test-CharsInPath -Path $InCorrectFileNameString -SkipCheckCharsInFileName -verbose:$VerboseFunctionOutput | should be 0
+                
+            }
+            
+            It "Input is string, InCorrectFileNameString" {
+                
+                Test-CharsInPath -Path $InCorrectFileNameString -verbose:$VerboseFunctionOutput | should be 3
+                
+            }
+            
+            It "Input is string, SkipCheckCharsInPath and SkipCheckCharsInFileName, InCorrectPathString" {
+                
+                Test-CharsInPath -Path $CorrectPathString -SkipCheckCharsInFileName -SkipCheckCharsInPath -verbose:$VerboseFunctionOutput | should be 1
+                
+            }
+            
+        }
+        
+        Context "Input is other than string of System.IO.X" {
+            
+            It "Input is Int32" {
+                
+                [Int]$PathToTest = 23
+                
+                { Test-CharsInPath -Path $PathToTest -verbose:$VerboseFunctionOutput }  | should Throw
+                
+            }
             
         }
         
     }
     
-    Context "Input is a string" {
-        
-        [String]$CorrectPathString = 'C:\Windows\Temp\Add-ADGroupMember.ps1'
-        
-        [String]$InCorrectPathString = 'C:\Windows\Te%mp\Add-ADGroupMember.ps1'
-        
-        [String]$InCorrectFileNameString = 'C:\Windows\Temp\Add-ADGrou/pMember.ps1'
-        
-        It "Input is string, CorrectPathString" {
-            
-            Test-CharsInPath -Path $CorrectPathString | should be 0
-            
-        }
-            
-        It "Input is string, SkipCheckCharsInPath, CorrectPathString" {
-            
-            Test-CharsInPath -Path $CorrectPathString -SkipCheckCharsInPath | should be 0
-            
-        }
-        
-        It "Input is string, SkipCheckCharsInFileName, CorrectPathString" {
-            
-            Test-CharsInPath -Path $CorrectPathString -SkipCheckCharsInFileName | should be 0
-            
-        }
-        
-        It "Input is string, InCorrectPathString" {
-            
-            Test-CharsInPath -Path $InCorrectPathString -SkipCheckCharsInPath | should be 2
-            
-        }
-        
-        
-        It "Input is string, SkipCheckCharsInPath, InCorrectPathString" {
-            
-            Test-CharsInPath -Path $InCorrectPathString -SkipCheckCharsInPath | should be 2
-            
-        }
-        
-        It "Input is string, SkipCheckCharsInFileName, InCorrectPathString" {
-            
-            Test-CharsInPath -Path $InCorrectPathString -SkipCheckCharsInFileName | should be 2
-            
-        }
-        
-    }
-    
-    #>
-}
--- Functions/Test-CharsInPath.ps1
+++ Functions/Test-CharsInPath.ps1
@@ -10,7 +10,9 @@
     
     - 0 - everything OK
     - 1 - nothing to check
-    - 
+    - 2 - an incorrect char found in the path part
+    - 3 - an incorrect char found in the file name part
+    - 4 - incorrect chars found in the path part and in the file name part
 
     .PARAMETER Path
     
@@ -37,11 +39,16 @@
 
     VERSIONS HISTORY
     - 0.1.0 - 2016-06-06 - Initial release
-    - 0.2.0 - 2016-06-06 - The second draft
+    - 0.2.0 - 2016-06-06 - The second draft, Pester tests updated
+    - 0.3.0 - 2016-06-07 - The first working version :-)
+    
+    TODO
+    - update help
 
 #>
     
     [cmdletbinding()]
+    [OutputType([System.Int32])]
     param (
         
         [parameter(Mandatory = $true)]
@@ -54,13 +61,15 @@
         
     )
     
-    
-    
     $PathInvalidChars = [System.IO.Path]::GetInvalidPathChars() #36 chars
     
-    $FileInvalidChars = [System.IO.Path]::GetInvalidFileNameChars() #41 chars
+    $FileNameInvalidChars = [System.IO.Path]::GetInvalidFileNameChars() #41 chars
     
-    $FileOnlyInvalidChars =@(':','*','?','\','/') #5 chars - as a difference
+    #$FileOnlyInvalidChars = @(':', '*', '?', '\', '/') #5 chars - as a difference
+    
+    $IncorectCharFundInFileName = $false
+    
+    $IncorectCharFundInPath = $false
     
     $PathType = ($Path.GetType()).Name
     
@@ -83,17 +92,114 @@
     
     ElseIf ($PathType -eq 'String') {
         
+        #Convert String to Array of chars
+        $PathArray = $Path.ToCharArray()
+        
+        $PathLength = $PathArray.Length
+        
+        [array]::Reverse($PathArray)
+        
+        For ($i = 0; $i -lt $PathLength; $i++) {
+            
+            If (@('\', '/') -contains $PathArray[$i]) {
+                
+                [String]$DirectoryPath = [String]$Path.Substring(0, $($PathArray.Length - $i))
+                                
+                break
+                
+            }
+            
+        }
+        
+        If ([String]::IsNullOrEmpty($DirectoryPath)) {
+            
+            [String]$FileName = [String]$Path
+            
+        }
+        Else {
+            
+            [String]$FileName = $Path.Replace($DirectoryPath, "")
+            
+        }
+        
+        
+        [String]$MessageText = "The path provided as a string was devided to, directory part: {0} ; file name part: {1}" -f $DirectoryPath, $FileName
+        
+        Write-Verbose -Message $MessageText
+        
+        If ($SkipCheckCharsInPath.IsPresent -and $SkipCheckCharsInFileName.IsPresent) {
+            
+            Return 1
+            
+        }
+        
+        
+        If (-not ($SkipCheckCharsInPath.IsPresent)) {
+            
+            foreach ($Char in $PathInvalidChars) {
+                
+                If ($DirectoryPath.ToCharArray() -contains $Char) {
+                    
+                    $IncorectCharFundInPath = $true
+                    
+                    [String]$MessageText = "The incorrect char {0} with the UTF code {1} found in the Path part" -f $Char, $([int][char]$Char)
+                    
+                    Write-Verbose -Message $MessageText
+                                        
+                }
+                
+            }
+            
+        }
+        
+        If (-not ($SkipCheckCharsInFileName.IsPresent)) {
+            
+            foreach ($Char in $FileNameInvalidChars) {
+                                
+                If ($FileName.ToCharArray() -contains $Char) {
+                    
+                    $IncorectCharFundInFileName = $true
+                    
+                    [String]$MessageText = "The incorrect char {0} with the UTF code {1} found in FileName part" -f $Char, $([int][char]$Char) 
+                    
+                    Write-Verbose -Message $MessageText
+                    
+                }
+                
+            }
+            
+        }
+        
     }
     
     Else {
         
+        [String]$MessageText = "Input object {0} can't be tested" -f ($Path.GetType()).Name
         
+        Throw $MessageText
         
     }
     
+    If ($IncorectCharFundInPath -and $IncorectCharFundInFileName) {
+        
+        Return 4
+        
+    }
+    elseif ($IncorectCharFundInPath) {
+        
+        Return 2
+        
+    }
     
+    elseif ($IncorectCharFundInFileName) {
+        
+        Return 3
+        
+    }
+    Else {
+        
+        Return 0
+        
+    }
     
 }
-
-
-
